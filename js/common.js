8e3a12c31fd5db9fe2b2e964dd4fa481e76a3e2c
pascal2402 <118438240+pascal2402@users.noreply.github.com>
2022-12-01 12:04:31 +0100

    1 file modified

--- js/common.js
+++ js/common.js
@@ -1,29 +1,29 @@
 /**
- * Load stored counts from local storage, if some are stored there.
+ * Load stored entries from local storage, if some are stored there.
  *
- * @returns {*[]|any} Stored counts from local storage or empty array, if no counts were present.
+ * @returns {*[]|any} Stored entries from local storage or empty array, if no entries are present.
  */
- function loadStoredCounts() {
-  var storedCountsJson = localStorage.getItem("counts");
-  if (storedCountsJson) {
-      var counts = JSON.parse(storedCountsJson);
-      console.debug(`Count of loaded counts: ${counts.length}`);
-      return counts;
+ function loadStoredEntries() {
+  var storedEntriesJson = localStorage.getItem("entries");
+  if (storedEntriesJson) {
+      var entries = JSON.parse(storedCountsJson);
+      console.debug(`Count of loaded entries: ${entries.length}`);
+      return entries;
   }
 
   return [];
 }
 
 /**
-* Store counts in the local storage.
+* Store entries in the local storage.
 *
-* @param counts Counts to store.
+* @param entries Entries to store.
 */
-function storeCounts(counts) {
-  if (counts) {
-      localStorage.setItem("counts", JSON.stringify(counts));
-      console.debug(`Count of stored counts: ${counts.length}`);
+function storeEntries(entries) {
+  if (entries) {
+      localStorage.setItem("entries", JSON.stringify(entries));
+      console.debug(`Count of stored entries: ${entries.length}`);
   } else {
-      console.error("No counts to store");
+      console.error("No entries to store");
   }
 }
